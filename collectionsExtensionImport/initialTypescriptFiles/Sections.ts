3ab11f100bf2bba5a8a720c4de900a5ee1883a3f
Marthinus Engelbrecht <marthinus.engelbrecht@protonmail.com>
2018-06-04 16:13:01 +0200

    Fix wrong Array type

--- collectionsExtensionImport/initialTypescriptFiles/Sections.ts
+++ collectionsExtensionImport/initialTypescriptFiles/Sections.ts
@@ -3,7 +3,7 @@ type SectionValueType = (string | number | { number: number } | { tool: string }
 export const Sections = new Map<string, SectionValueType>(new Array<any>(
     new Array<string | SectionValueType>(
         "Groceries",
-        new Array<MapValueType>("Item1", "Item2", {number: 3}, 4),
+        new Array<SectionValueType>("Item1", "Item2", {number: 3}, 4),
         new Array<string | SectionValueType>(
             "Tools",
             new Array<SectionValueType>("Tool1", {tool: "Other"}, 5, 6, "Tool7")
