4de60fb313d8fd717bbb4cfd28b6a3b972866b50
Marthinus Engelbrecht <marthinus.engelbrecht@protonmail.com>
2018-06-06 19:46:35 +0200

    Fix invalid types declartion in Map when using new Array expressions

--- collectionsExtensionImport/expectedTypescriptFiles/Sections.ts
+++ collectionsExtensionImport/expectedTypescriptFiles/Sections.ts
@@ -3,14 +3,16 @@ import {Array, Map} from "@typical-linguist/collections-extension";
 type UnionReferenceType = string | number | { number: number } | { tool: string }
 type SectionValueType = (UnionReferenceType)[];
 
-export const Sections = new Map<string, SectionValueType>(new Array<any>(
-    new Array<string | SectionValueType>(
-        "Groceries",
-        new Array<SectionValueType>("Item1", "Item2", {number: 3}, 4),
+export const Sections = new Map<string, SectionValueType>(
+    new Array<any>(
+        new Array<string | SectionValueType>(
+            "Groceries",
+            new Array<UnionReferenceType>("Item1", "Item2", {number: 3}, 4),
+        ),
         new Array<string | SectionValueType>(
             "Tools",
-            new Array<SectionValueType>("Tool1", {tool: "Other"}, 5, 6, "Tool7")
-        )
-    )
-));
+            new Array<UnionReferenceType>("Tool1", {tool: "Other"}, 5, 6, "Tool7"),
+        ),
+    ),
+);
 
