06b981baa34b3c2afe10d9f45edb488432d5803f
Marthinus Engelbrecht <marthinus.engelbrecht@protonmail.com>
2018-06-07 16:31:14 +0200

    Remove trailing commas

--- collectionsExtensionImport/expectedTypescriptFiles/Sections.ts
+++ collectionsExtensionImport/expectedTypescriptFiles/Sections.ts
@@ -7,12 +7,12 @@ export const Sections = new Map<string, SectionValueType>(
     new Array<any>(
         new Array<string | SectionValueType>(
             "Groceries",
-            new Array<UnionReferenceType>("Item1", "Item2", {number: 3}, 4),
+            new Array<UnionReferenceType>("Item1", "Item2", {number: 3}, 4)
         ),
         new Array<string | SectionValueType>(
             "Tools",
-            new Array<UnionReferenceType>("Tool1", {tool: "Other"}, 5, 6, "Tool7"),
-        ),
-    ),
+            new Array<UnionReferenceType>("Tool1", {tool: "Other"}, 5, 6, "Tool7")
+        )
+    )
 );
 
